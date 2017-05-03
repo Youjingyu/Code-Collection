14e2600b60c9134144292fee664fedc59f2df596
youjingyu <735284268@qq.com>
2017-05-03 11:33:39 +0800

    add deep data getting function

---
+++ smartCode/getDeepObjectData.js
@@ -0,0 +1,40 @@
+/**
+ * 获取深层数据结构中的数据
+ * @param {Array} keyArr 查找数据所需key组成的数组
+ * @param {Object} dataObject 旋转完成后的回调函数
+ */
+function getDeepObjectData(keyArr, dataObject) {
+    return keyArr.reduce(function (data, key) {
+        return (data && data[key]) ? data[key] : null
+    }, dataObject)
+}
+
+// 使用示例
+var testData = {
+    a: {
+        b: {
+            c:{
+              d: 2,
+              h: 5
+            },
+            j: 4
+        },
+        f:{
+            g: 1
+        }
+    },
+    r: {
+        l:{
+            o: 2
+        }
+    }
+}
+
+// 通常做法
+testData['a'] &&
+testData['a']['b'] &&
+testData['a']['b']['c'] &&
+testData['a']['b']['c']['h']
+
+// 使用上述函数
+getDeepObjectData(['a', 'b', 'c', 'h'], testData) // 5
